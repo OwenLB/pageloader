b1f9f687f2f5119353c129167585cb9a356eb8a0
Owen <lebec.owen@yahoo.fr>
2023-07-28 17:30:21 +0200

    animate header + banner

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -30,40 +30,36 @@ const item = {
   },
 };
 
-// const itemMain = {
-//   hidden: { opacity: 0, y: 200 },
-//   show: {
-//     opacity: 1,
-//     y: 0,
-//     transition: {
-//       ease: [0.6, 0.01, 0.05, 0.95],
-//       duration: 1.6,
-//     },
-//   },
-// };
+const itemMain = {
+  hidden: { opacity: 0, y: 200 },
+  show: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      ease: [0.6, 0.01, 0.05, 0.95],
+      duration: 1.6,
+    },
+  },
+};
 
 const Loader = ({ setLoading }) => {
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 4000);
-    return () => clearTimeout(timer);
-  });
-
   return (
     <div className='loader'>
       <motion.div className='loader-inner'
       variants={container}
       initial='hidden'
       animate='show'
-      exit='exit'>
+      exit='exit'
+      onAnimationComplete={() => setLoading(false)}>
         <ImageBlock variants={item} id='image-1' />
-        <div className='transition-image'>
-          <img
+        <motion.div className='transition-image'
+        variants={itemMain}>
+          <motion.img
             src={process.env.PUBLIC_URL + `/images/image-2.jpg`}
             alt='random alt'
+            layoutId="main-image-1"
           />
-        </div>
+        </motion.div>
         <ImageBlock variants={item} id='image-3' />
         <ImageBlock variants={item} id='image-4' />
         <ImageBlock variants={item} id='image-5' />
